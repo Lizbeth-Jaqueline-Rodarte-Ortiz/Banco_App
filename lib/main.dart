672f5a5e25bb8c6ee3a25f589abf8adcc0d86af8
FlutLab.io <>
2022-05-03 14:18:52 +0000

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(BancoApp());
+} //funcion principal
+
+class BancoApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'App Banco',
+        theme: ThemeData(
+          primarySwatch: Colors.green,
+        ),
+        home: PaginaInical());
+  }
+}
+
+class PaginaInical extends StatefulWidget {
+  PaginaInical({Key? key}) : super(key: key);
+  @override
+  _PaginaInicalState createState() => _PaginaInicalState();
+}
+
+class _PaginaInicalState extends State<PaginaInical> {
+  List<String> images = [
+    "assets/images/bbva.jpg",
+    "assets/images/bbva1.jpg",
+    "assets/images/bbva2.jpg",
+    "assets/images/bbva3.jpg",
+    "assets/images/bbva4.jpg",
+    "assets/images/bbva.jpg",
+    "assets/images/bbva2.jpg",
+    "assets/images/bbva3.jpg",
+    "assets/images/bbva.jpg",
+    "assets/images/bbva1.jpg",
+    "assets/images/bbva2.jpg",
+    "assets/images/bbva3.jpg",
+    "assets/images/bbva4.jpg",
+    "assets/images/bbva.jpg",
+    "assets/images/bbva2.jpg",
+    "assets/images/bbva3.jpg",
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Flutter GridView"),
+        ),
+        body: GridView.custom(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+          ),
+          childrenDelegate: SliverChildBuilderDelegate(
+            (BuildContext, index) {
+              return Image.asset(
+                images[index],
+                fit: BoxFit.cover,
+              );
+            },
+            childCount: 12,
+          ),
+          padding: EdgeInsets.all(10),
+          shrinkWrap: true,
+        ));
+  }
+}
